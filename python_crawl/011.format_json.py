62e9c3eeb50d5468bf3ca931ccecfc62cad434c1
htwoo <1405360652@qq.com>
2019-09-18 13:23:32 +0800

    4-1、贴吧爬虫

--- python_crawl/011.format_json.py
+++ python_crawl/011.format_json.py
@@ -1,13 +1,8 @@
-import json
-import re
-# import requests
-from ten_requests_skills import parse_url
-from pprint import pprint
-
-url = "https://pm.3.cn/prices/mgets?origin=3&ext=01000000&skuids=1399903,27269595515,42010078901,2372300,100006388460,41220088267,2502890,35509125811,11803445806,25786381619&source=fashionv4"
-html_str = parse_url(url)
-# print(html_str)
-ret1 = json.loads(html_str)
-pprint(ret1)
-print(type(ret1))
-
+# 选择元素
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'lxml')
+print(type(soup.title))  # 返回bs4类：元素标签
+print(soup.title)
+print(soup.head)
+print(soup.p)
+soup.
