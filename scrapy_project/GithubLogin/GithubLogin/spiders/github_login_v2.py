814461cca338e292bab4bb86e82da4a0fc46b3f1
htwoo <1405360652@qq.com>
2019-10-05 13:22:39 +0800

    github login version 2

---
+++ scrapy_project/GithubLogin/GithubLogin/spiders/github_login_v2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+
+class GithubLoginV2Spider(scrapy.Spider):
+    name = 'github_login_v2'
+    allowed_domains = ['github.com']
+    start_urls = ['https://github.com/login']
+
+    def parse(self, response):
+        yield scrapy.FormRequest.from_response(
+            response,
+            formdata={
+                "login":"htwoo111",
+                "password":"a201741501113"
+                    },
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        print('in after login function')
+        name = re.findall(r'htwoo', response.body.decode())
+        print(name)
