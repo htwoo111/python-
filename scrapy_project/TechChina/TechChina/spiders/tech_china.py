a2ea59fb3e48e1682e7451b28cff02968c31c162
htwoo <1405360652@qq.com>
2019-10-05 09:16:46 +0800

    tech_china in loaderItem

--- scrapy_project/TechChina/TechChina/spiders/tech_china.py
+++ scrapy_project/TechChina/TechChina/spiders/tech_china.py
@@ -2,8 +2,8 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from TechChina.loader import TechChinaLoader
 from TechChina.items import TechchinaItem
-
 
 class TechChinaSpider(CrawlSpider):
     name = 'tech_china'
@@ -18,15 +18,26 @@ class TechChinaSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        item = TechchinaItem()
-        item['title'] = response.xpath(
-            '//h1[@id="chan_newsTitle"]/text()').extract_first()
-        item['datetime'] = response.xpath(
-            '//div[@id="chan_newsInfo"]/text()').re_first(r'\d{4}-\d{2}-\d{2}.*\d')
-        # item['text'] = ''.join(response.xpath('//div[@id="chan_newsDetail"]//text()').extract()).strip()
-        item['url'] = response.url
-        item['website'] = '中华网'
-        item['source'] = response.xpath(
-            '//div[@id="chan_newsInfo"]/text()').re_first(r'来源：(.*)').strip()
-        print(item)
-        yield item
+        loader = TechChinaLoader(item=TechchinaItem(), response=response)
+        loader.add_xpath('title', '//h1[@id="chan_newsTitle"]/text()')
+        loader.add_value('url', response.url)
+        # loader.add_xpath('text')
+        loader.add_xpath('datetime', '//div[@id="chan_newsInfo"]/text()', re=r'\d{4}-\d{2}-\d{2}.*\d')
+        loader.add_xpath('source', '//div[@id="chan_newsInfo"]/text()', re='来源：(.*)')
+        loader.add_value('website', '中华网')
+        print(loader.load_item())
+        yield loader.load_item()
+
+
+        # item = TechchinaItem()
+        # item['title'] = response.xpath(
+        #     '//h1[@id="chan_newsTitle"]/text()').extract_first()
+        # item['datetime'] = response.xpath(
+        #     '//div[@id="chan_newsInfo"]/text()').re_first(r'\d{4}-\d{2}-\d{2}.*\d')
+        # # item['text'] = ''.join(response.xpath('//div[@id="chan_newsDetail"]//text()').extract()).strip()
+        # item['url'] = response.url
+        # item['website'] = '中华网'
+        # item['source'] = response.xpath(
+        #     '//div[@id="chan_newsInfo"]/text()').re_first(r'来源：(.*)').strip()
+        # print(item)
+        # yield item
